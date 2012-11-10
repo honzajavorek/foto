6c7cae23c7aef7e53af9ff6f21694ae6d3bbf8a7
Honza Javorek <jan.javorek@gmail.com>
2012-11-10 18:56:23 +0100

    Main, argument parsing.

--- elk/main.py
+++ elk/main.py
@@ -4,23 +4,37 @@
 """Elk.
 
 Usage:
+    elk organize
+    elk auto
+
     elk upload
     elk sync
     elk video
+    elk panorama
     elk info
     elk captions
+
     elk wipe [ info | captions ]
+
     elk -h | --help
     elk --version
 
 Options:
+    organize            Take all mess in current directory and place
+                        it into directories where each of them represents
+                        a single day.
+    auto                Do everything automatically.
+
     upload              Upload to remote storages.
     sync                Synchronize captions and info with remote storages.
     video               Convert all videos.
+    panorama            Detect and convert all panoramas.
     info                Print info.
     captions            Print all captions.
+
     wipe info           Wipe info.
     wipe captions       Wipe all captions.
+
     -h --help           Show this screen.
     --version           Show version.
 """
